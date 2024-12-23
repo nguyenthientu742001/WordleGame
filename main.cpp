10a14d0e08ae9f5d03e73a94a35700cf9273fda0
nguyenthientu742001 <nguyenthientu12345@gmail.com>
2024-12-23 15:21:44 +0700

    Wordle Game Project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    string userName;
+
+    cout << "Welcome to Wordle Game" << endl;
+    cout << "Please input user name: ";
+    getline(cin, userName);
+
+
+    return 0;
+}
